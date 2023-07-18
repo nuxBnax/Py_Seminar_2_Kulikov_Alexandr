bfd6f1ea1ec202178db8d697211ad650a9d627e3
Kulikov Alexander <shouter84@yandex.ru>
2023-07-18 22:46:10 +0300

    Added solution for task_10

--- task_10.py
+++ task_10.py
@@ -7,3 +7,29 @@
 # же стороной. Выведите минимальное количество монет,
 # которые нужно перевернуть
 
+from random import randint
+
+n = int(input("Enter the number of coins: "))
+
+eagle_counter = 0
+tails_counter = 0
+for _ in range(n):
+    droping = randint(0, 1)
+    print(droping)
+    if droping == 0:
+        tails_counter += 1
+    elif droping == 1:
+        eagle_counter += 1
+print('Coins with tails: ', tails_counter)
+print('Coins with eagle: ', eagle_counter)
+
+if tails_counter < eagle_counter:
+    print('There are fewer tails, it is easier to flip coins with tails')
+elif tails_counter > eagle_counter:
+    print('There are fewer eagle, it is easier to flip coins with eagle')
+else:
+    print('Coins with eagle and tails are equal, you can flip any of them')
+
+
+
+
